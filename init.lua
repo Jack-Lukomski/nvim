a6e14f2e6583524829906a422b4931d46734993f
Jack Lukomski <jtlukomski@gmail.com>
2023-12-04 19:57:08 -0500

    Init

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("core.keymaps")
+require("core.plugins")
+require("core.pluginconfig.nvim-tree")
+require("core.pluginconfig.dracula")
+require("core.pluginconfig.treesitter")
+require("core.pluginconfig.telescope")
+require("core.pluginconfig.nvim-lspconfig")
