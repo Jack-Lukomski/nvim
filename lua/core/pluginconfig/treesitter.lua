edc09b949bb52b33275027c7aa72215cc112c631
jack <jtlukomski@gmail.com>
2024-01-09 11:46:00 -0500

    updates

--- lua/core/pluginconfig/treesitter.lua
+++ lua/core/pluginconfig/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "lua", "vim", "python"},
+    ensure_installed = {"c", "lua", "vim", "python", "rust"},
     sync_install = false,
     auto_install = false,
     highlight = {
